8824b2c080d64fdda58d2e9c7aca123b604916e9
Lucas Santana Lemos <lslemos59@gmail.com>
2023-07-05 22:37:24 -0300

    kinda cool

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,13 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("N1: ");
-        float n1 = scanner.nextFloat();
-        System.out.println("N2: ");
-        float n2 = scanner.nextFloat();
-
-        float media = (n1 + n2) / 2;
-        String situacao;
-
-        if (media > 6){
-            situacao = "aprovado";
-        } else if (media == 6){
-            situacao = "passou se arrastando";
-        } else{
-            situacao = "reprovado!!";
+        String[][] frutas = {{"Banana", "Morango"}, {"Abacate", "Melancia"}};
+        int index = 0;
+        while (index < frutas.length) {
+            for (String item : frutas[index]) {
+                System.out.println(item.toUpperCase());
+            }
+            index++;
         }
-
-        System.out.println("Sua média foi de: " + media);
-        System.out.println("Sua situação: " + situacao.toUpperCase());
-
     }
 }
