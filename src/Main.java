cffd198cff1cbe715893432a876ce264b2bb903e
Lucas Santana Lemos <lslemos59@gmail.com>
2023-07-06 21:39:38 -0300

    game

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,36 @@
-import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> itemsList = new ArrayList<String>();
+        Random random = new Random();
+        ArrayList<String> jogo = new ArrayList<String>();
+        jogo.add("pedra");
+        jogo.add("papel");
+        jogo.add("tesoura");
 
+        System.out.println("BEM VINDO AO JOGO PEDRA, PAPEL OU TESOURA\nFAÇA SUA JOGADA: ");
+        System.out.println("[0] PEDRA\n[1] PAPEL\n[2] TESOURA");
+        int user_Choice = scanner.nextInt();
+        int maquina_Choice = random.nextInt(2);
 
-        boolean continuar = true;
-        int i = 5;
-        while (continuar) {
-            System.out.println("Adicione objetos a uma lista: ");
-            String object = scanner.nextLine();
-            itemsList.add(object.toUpperCase());
+        System.out.println("VOCÊ ESCOLHEU: " + jogo.get(user_Choice).toUpperCase());
+        System.out.println("MAQUINA ESCOLHEU: " + jogo.get(maquina_Choice).toUpperCase());
 
-            System.out.println("Objeto adicionado! deseja continuar? [S/N]");
-            String escolha = scanner.nextLine();
-
-            if (escolha.toUpperCase().equals("N")) {
-                continuar = false;
-            }
-        }
-        for (String item : itemsList){
-            System.out.println(item);
+        if (user_Choice == maquina_Choice) {
+            System.out.println("EMPATE!!");
+        } else if (jogo.get(user_Choice).equals("pedra") && jogo.get(maquina_Choice).equals("tesoura")){
+            System.out.println("VOCÊ VENCEU!");
+        } else if (jogo.get(user_Choice).equals("tesoura") && jogo.get(maquina_Choice).equals("papel")){
+            System.out.println("VOCÊ VENCEU!");
+        } else if (jogo.get(user_Choice).equals("papel") && jogo.get(maquina_Choice).equals("pedra")){
+            System.out.println("VOCÊ VENCEU!!");
+        } else{
+            System.out.println("VOCÊ PERDEU!");
         }
     }
 }
