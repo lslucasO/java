324cf02c92b1bafad5ad7a970fd24a3393d2488e
Lucas Santana Lemos <lslemos59@gmail.com>
2023-07-06 22:17:59 -0300

    Using functions

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,51 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
 public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        ArrayList<String> jogo = new ArrayList<String>();
+    private static ArrayList<String> jogo = new ArrayList<String>(); /* Variavel Global */
+    private static Scanner scanner = new Scanner(System.in);
+    private static Random random = new Random();
+    private static int user_Choice;
+    private static int maquina_Choice;
+
+    static void createArray() { /* Função */
         jogo.add("pedra");
         jogo.add("papel");
         jogo.add("tesoura");
-
+    }
+    static void randomizeChoice(){
+        user_Choice = scanner.nextInt();
+        maquina_Choice = random.nextInt(2);
+    }
+    static void announceGame(){
         System.out.println("BEM VINDO AO JOGO PEDRA, PAPEL OU TESOURA\nFAÇA SUA JOGADA: ");
         System.out.println("[0] PEDRA\n[1] PAPEL\n[2] TESOURA");
-        int user_Choice = scanner.nextInt();
-        int maquina_Choice = random.nextInt(2);
-
+    }
+    static void gameShow(){
         System.out.println("VOCÊ ESCOLHEU: " + jogo.get(user_Choice).toUpperCase());
         System.out.println("MAQUINA ESCOLHEU: " + jogo.get(maquina_Choice).toUpperCase());
-
+    }
+    static void gameResults(){
         if (user_Choice == maquina_Choice) {
             System.out.println("EMPATE!!");
-        } else if (jogo.get(user_Choice).equals("pedra") && jogo.get(maquina_Choice).equals("tesoura")){
+        } else if (jogo.get(user_Choice).equals("pedra") && jogo.get(maquina_Choice).equals("tesoura")) {
             System.out.println("VOCÊ VENCEU!");
-        } else if (jogo.get(user_Choice).equals("tesoura") && jogo.get(maquina_Choice).equals("papel")){
+        } else if (jogo.get(user_Choice).equals("tesoura") && jogo.get(maquina_Choice).equals("papel")) {
             System.out.println("VOCÊ VENCEU!");
-        } else if (jogo.get(user_Choice).equals("papel") && jogo.get(maquina_Choice).equals("pedra")){
+        } else if (jogo.get(user_Choice).equals("papel") && jogo.get(maquina_Choice).equals("pedra")) {
             System.out.println("VOCÊ VENCEU!!");
-        } else{
+        } else {
             System.out.println("VOCÊ PERDEU!");
         }
+    }
+
+    public static void main(String[] args){
+
+        createArray();
+        announceGame();
+        randomizeChoice();
+        gameShow();
+        gameResults();
+
     }
 }
